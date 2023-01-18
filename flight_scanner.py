c322257af4c74bf146b5d7b5380a4f2f5a78fc9e
Soren Umstot <soren.umstot@gmail.com>
2023-01-18 10:12:06 +0900

    Initial commit for my flight scanner script

---
+++ flight_scanner.py
@@ -0,0 +1,7 @@
+from time import sleep, strftime
+from random import randint
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import smtplib
+from email.mime.multipart import MIMEMultipart
