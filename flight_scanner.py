b6a98870e5b6da9b1224966d2fe952292a22d9e5
Soren Umstot <soren.umstot@gmail.com>
2023-01-19 13:49:03 +0900

    Project is returning list of flights scraped from kayak

--- flight_scanner.py
+++ flight_scanner.py
@@ -14,6 +14,19 @@ option = webdriver.ChromeOptions()
 option.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
 
 
-browser = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=option)
-browser.get("https://www.google.com")
-print(browser.title)
+browser = webdriver.Chrome(executable_path=chromedriver_path, options=option)
+
+
+kayak = 'https://www.kayak.com/flights/OSA-SAN/2023-02-17-flexible-3days/2023-02-24-flexible-3days?'
+
+browser.get(kayak)
+
+cheap_results = '//a[@data-code ="price"]'
+browser.find_element("xpath", cheap_results).click()
+sleep(3)
+
+xp_results_table = '//*[@class = "resultWrapper"]'
+flight_containers = browser.find_elements("xpath", xp_results_table)
+flights_list = [flight.text for flight in flight_containers]
+
+print(flights_list)
