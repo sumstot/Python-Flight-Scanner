fd371827202340d7410272b3d692aaecd8284279
Soren Umstot <soren.umstot@gmail.com>
2023-01-19 11:46:21 +0900

    Got selenium working on the project

--- flight_scanner.py
+++ flight_scanner.py
@@ -3,5 +3,17 @@ from random import randint
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 import smtplib
 from email.mime.multipart import MIMEMultipart
+
+chromedriver_path = '/usr/local/bin/chromedriver'
+
+option = webdriver.ChromeOptions()
+
+option.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+
+
+browser = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=option)
+browser.get("https://www.google.com")
+print(browser.title)
